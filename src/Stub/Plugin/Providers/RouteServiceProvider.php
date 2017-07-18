3c6e8b8a82b13b457158975126b7b3d538b09030
Menq <sahak@ukdevplus.co.uk>
2017-07-18 21:05:08 +0400

    transfer files

--- src/Stub/Plugin/Providers/RouteServiceProvider.php
+++ src/Stub/Plugin/Providers/RouteServiceProvider.php
@@ -14,7 +14,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    protected $namespace = 'App\Modules\Tools\Http\Controllers';
+    protected $namespace = 'Sahakavatar\Tools\Http\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
